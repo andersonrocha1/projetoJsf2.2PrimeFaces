2499a3125ebfc7014ae4a66080d59c1b76ed7d4e
Anderson Rocha <anderson.tec.informacao@gmail.com>
2020-12-31 19:50:59 -0300

    Alguns ajustes no cadastro...

--- projetojsfprimefaces/src/br/com/project/bean/view/CidadeBeanView.java
+++ projetojsfprimefaces/src/br/com/project/bean/view/CidadeBeanView.java
@@ -1,5 +1,6 @@
 package br.com.project.bean.view;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
@@ -25,7 +26,10 @@ public class CidadeBeanView extends BeanManagedViewAbstract {
 
 	private static final long serialVersionUID = 1L;
 	
+	private String url = "/cadastro/cad_cidade.jsf?faces-redirect=true";
 	
+	private List<Cidade> list = new ArrayList<Cidade>();
+		
 	private Cidade objetoSelecionado = new Cidade();
 	
 	@Autowired
@@ -37,6 +41,31 @@ public class CidadeBeanView extends BeanManagedViewAbstract {
 		objetoSelecionado = cidadeController.merge(objetoSelecionado);
 		
 		return "";
+	}
+	
+	@Override
+	public String novo() throws Exception {
+		objetoSelecionado = new Cidade();
+		return url;
+	}
+	
+	@Override
+	public String editar() throws Exception {
+		
+		return url;
+	}
+	
+	@Override
+	public void excluir() throws Exception {
+		
+		cidadeController.delete(objetoSelecionado);
+		novo();
+	}
+	
+	public List<Cidade> getList() throws Exception {
+		
+		list = cidadeController.findList(Cidade.class);
+		return list;
 	}
 
 	public Cidade getObjetoSelecionado() {
